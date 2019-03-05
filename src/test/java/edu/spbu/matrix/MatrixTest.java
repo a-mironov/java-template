789a7fb3221f21feb54ee3dc60318c7033532219
Artem Mironov <st063750@student.spbu.ru>
2019-03-05 22:12:16 +0300

    Multithreaded multiplication fixed now

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -38,4 +38,36 @@ public class MatrixTest
       SparseMatrix actual = (SparseMatrix) m1.mul(m2);
       assertEquals(expected, actual);
   }
+  /* parallel multiplication tests */
+  @Test
+  public void dmulDD() {
+      Matrix m1 = new DenseMatrix("m1.txt");
+      Matrix m2 = new DenseMatrix("m2.txt");
+      Matrix expected = new DenseMatrix("result.txt");
+      assertEquals(expected, m1.dmul(m2));
+  }
+  @Test
+  public void dmulDS() {
+      Matrix m1 = new DenseMatrix("m1.txt");
+      Matrix m2 = new SparseMatrix("m2.txt");
+      Matrix expected = new DenseMatrix("result.txt");
+      Matrix actual = m1.dmul(m2);
+      assertEquals(expected, actual);
+      //assertEquals(expected, m1.dmul(m2));
+  }
+  @Test
+  public void dmulSD() {
+      Matrix m1 = new SparseMatrix("m1.txt");
+      Matrix m2 = new DenseMatrix("m2.txt");
+      Matrix expected = new DenseMatrix("result.txt");
+      assertEquals(expected, m1.dmul(m2));
+  }
+  @Test
+  public void dmulSS() {
+      Matrix m1 = new SparseMatrix("m1.txt");
+      Matrix m2 = new SparseMatrix("m2.txt");
+      Matrix expected = new SparseMatrix("result.txt");
+      SparseMatrix actual = (SparseMatrix) m1.dmul(m2);
+      assertEquals(expected, actual);
+  }
 }
